4766b719e0c9cb864b83d38d014201fb723c8edc
MikaXII <mikael.joubert@supinfo.com>
2015-08-26 15:05:05 +0200

    first commit

---
+++ recalbot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# -*- coding: utf8 -*-
+
+import irclib
+import ircbot
+import settings
+
+class Recalbot(ircbot.SingleServerIRCBot):
+    def __init__(self):
+        ircbot.SingleServerIRCBot.__init__(self, [(settings.SRV, settings.PORT, settings.PWD)],settings.NAME, settings.DESC)
+
+    def on_welcome(self, serv, ev):
+	#Auth needed
+        serv.join("#test-recalbot")
+
+    def on_pubmsg(self, serv, ev):
+        auteur = irclib.nm_to_n(ev.source())
+        canal = ev.target()
+        message = ev.arguments()[0].lower() 
+
+if __name__ == "__main__":
+    Recalbot().start()
+
